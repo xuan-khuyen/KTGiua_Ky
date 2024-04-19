aacc4a6430e8c9c08e1136cdce02d2fd5a3068e2
Le Thu Nguyen <10757059+nglthu@users.noreply.github.com>
2024-04-19 08:03:56 +0000

    change index

--- index.php
+++ index.php
@@ -1,10 +1,5 @@
 <?php
 
-///*----------------------------------------------------------------------------------------
-// * Copyright (c) Microsoft Corporation. All rights reserved.
-// * Licensed under the MIT License. See LICENSE in the project root for license information.
-// *---------------------------------------------------------------------------------------*/
-
 function sayHello($name) {
 	echo "Hello $name!";
 }
@@ -17,13 +12,9 @@ function sayHello($name) {
 	</head>
 	<body>
 		<?php 
-		
-		sayHello('K15-N03 2024');
-			
-	
-			
+		sayHello('K15-N03 2024');		
 		?>
-		<form action="checkLogin.phd" method="POST">
+		<form action="checkLogin.php" method="POST">
 		<a>Username:</a><input type=text name="userName" size =16>
 		<a>Password:</a><input type=password name="passWord" size =16>
 		<input type=submit name=submit value="Login">
