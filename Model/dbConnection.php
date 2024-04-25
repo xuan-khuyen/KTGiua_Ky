726713a584f957e141e3f59dcf83bf9c7c3d1031
Le Thu Nguyen <10757059+nglthu@users.noreply.github.com>
2024-04-25 11:58:27 +0000

    .env

--- Model/dbConnection.php
+++ Model/dbConnection.php
@@ -1,20 +1,35 @@
 
+
 <?php
-$database = 'SAMPLE';
-$user = 'db2inst1';
-$password = 'ibmdb2';
-$hostname = 'localhost';
-$port = 50000;
+$env = file_get_contents(".env");
 
-$conn_string = "DRIVER={IBM DB2 ODBC DRIVER};DATABASE=$database;" .
-  "HOSTNAME=$hostname;PORT=$port;PROTOCOL=TCPIP;UID=$user;PWD=$password;";
-$conn = db2_connect($conn_string, '', '');
+$lines = explode("\n",$env);
 
-if ($conn) {
-    echo "Connection succeeded.";
-    db2_close($conn);
-}
-else {
-    echo "Connection failed.";
-}
+foreach($lines as $line){
+  preg_match("/([^#]+)\=(.*)/",$line,$matches);
+  if(isset($matches[2])){
+    putenv(trim($line));
+  }
+} 
+
+$db = getenv('Database');//returns: localhost
+echo $db;
+/*
+
+$dbconn = pg_connect("dbname=.env.");
+//connect to a database named "mary"
+
+$dbconn2 = pg_connect("host=localhost port=5432 dbname=mary");
+// connect to a database named "mary" on "localhost" at port "5432"
+
+$dbconn3 = pg_connect("host=sheep port=5432 dbname=mary user=lamb password=foo");
+//connect to a database named "mary" on the host "sheep" with a username and password
+
+$conn_string = "host=sheep port=5432 dbname=test user=lamb password=bar";
+$dbconn4 = pg_connect($conn_string);
+//connect to a database named "test" on the host "sheep" with a username and password
+
+$dbconn5 = pg_connect("host=localhost options='--client_encoding=UTF8'");
+//connect to a database on "localhost" and set the command line parameter which tells the encoding is in UTF-8
+*/
 ?>
