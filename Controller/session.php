6feb0cdf518f899cbe6ed7f1eb7e10fde24b51ef
Le Thu Nguyen <10757059+nglthu@users.noreply.github.com>
2024-05-02 11:21:55 +0000

    counter

--- Controller/session.php
+++ Controller/session.php
@@ -1,8 +1,21 @@
 <?php
-
+/*
 session_start();
 $id = session_id();
 echo "Session Id: ".$id ;
+*/
+
+   session_start();
+   if( isset( $_SESSION['counter'] ) ) {
+      $_SESSION['counter'] += 1;
+   } else {
+      $_SESSION['counter'] = 1;
+   }
+   $msg = "Number of visits in this session: ".  $_SESSION['counter'];
+ 
+   echo "$msg"; 
+?>
+
 
 ?>
 <html>
