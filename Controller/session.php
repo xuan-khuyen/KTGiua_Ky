ad4c075caffecee2e7e412b927c3bd2978bf1e23
Le Thu Nguyen <10757059+nglthu@users.noreply.github.com>
2024-05-02 11:14:38 +0000

    session id

--- Controller/session.php
+++ Controller/session.php
@@ -1,16 +1,8 @@
 <?php
 
-$a = session_id();
-if ($a == '') session_start();
-//isset — Determine if a variable is declared and is different than null
-if (!isset($_SESSION['safety']))
-{
-    session_regenerate_id(true);
-   
-    $_SESSION['safety'] = true;
-    echo "Hello, session is " . " " . $safety;
-}
-$_SESSION['sessionid'] = session_id();
+session_start();
+$id = session_id();
+echo "Session Id: ".$id ;
 
 ?>
 <html>
